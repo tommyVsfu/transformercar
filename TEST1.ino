e6203afa249c8c21910e2804aa78e2dec09cf436
Tommy <tommy558788@gmail.com>
2017-07-07 19:34:46 +0800

    7/7 the car can avoid the obstacle from 30 cm and then turn right for 1sec delay, this version will be a version1, i will develop a backward to stop version2 next

--- TEST1.ino
+++ TEST1.ino
@@ -1,8 +1,13 @@
-/*7/6 vobotics code version
- *    馬達驅動電路，與前進code 完成
+
+/*7/7 vobotics code version
+ *   單純避障右轉 
  */
+#include <Ultrasonic.h>
+Ultrasonic ultrasonic(5,6);
+ 
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(9600);  
 pinMode(3,OUTPUT);
 pinMode(4,OUTPUT);
 pinMode(7,OUTPUT);
@@ -10,12 +15,30 @@ pinMode(8,OUTPUT);
 }
 
 void loop() {
-
-
   
-  // put your main code here, to run repeatedly:
+  double  cc;
+  cc = ultrasonic.distanceRead(CM);  // distance in CM
+  Serial.print(", CM: ");
+  Serial.print(cc);
+  if (cc <= 30) {
+    right();
+    delay(1000);
+  }
+  else {
+    moveforward();  //moveforeward 不能delay 不然他這樣下一次發送超音波會太慢
+  }
+
+
+}
+void moveforward() {
 digitalWrite(4,HIGH);
 digitalWrite(3,LOW);
 digitalWrite(8,HIGH);
 digitalWrite(7,LOW);
+}
+void right() {
+digitalWrite(4,HIGH);
+digitalWrite(3,LOW);
+digitalWrite(8,LOW);
+digitalWrite(7,HIGH);
 }
